e24be3c0acb9353033b21964fb6540f155a1cfb0
Ilya <malinin174@gmail.com>
2023-05-10 00:19:17 +0300

    Task 2.2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -35,11 +35,15 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
-
+    let discountSum = 0;
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-
+    if (productsSum >= discountMinSum) {
+        discountSum = productsSum / 100 * discountPart;
+    } else {
+        discountSum = 0;
+    }
     // Конец решения задания №2.2.
 
     return discountSum;
